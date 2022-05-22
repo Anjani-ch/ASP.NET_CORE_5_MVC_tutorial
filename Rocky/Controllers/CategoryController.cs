5f26de4ff1ba1df537b7a65004d8559d4100c101
Anjani <pranav.ch.12@outlook.com>
2022-05-22 18:30:58 +0200

    Initial Commit

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
+        }
+
+        // GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            Category obj;
+
+            if(id is null || id == 0)
+            {
+                return NotFound();
+            }
+
+            obj = _db.Categories.Find(id);
+
+            if(obj is null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            Category obj;
+
+            if (id is null || id == 0)
+            {
+                return NotFound();
+            }
+
+            obj = _db.Categories.Find(id);
+
+            if (obj is null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        // POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+
+            if (obj is null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Rocky/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rocky.Models;
+
+namespace Rocky.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Rocky.Models.ApplicationType> ApplicationType { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
